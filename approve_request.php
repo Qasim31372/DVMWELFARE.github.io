b4976e061674a1fb276d1ae378d5d249d1b1bae3
Qasim31372 <mq8840@gmail.com>
2026-03-04 02:34:15 -0800

    Delete approve_request.php

--- approve_request.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db.php';
-$id = $_GET['id'];
-$sql = "UPDATE farmers SET status='Approved' WHERE id=$id";
-if ($conn->query($sql)) { echo "Request approved!"; } else { echo "Error: ".$conn->error; }
-?>
